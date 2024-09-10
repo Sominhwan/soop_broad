e84f5221485c9bc7ef0e9e2fac401903998938a6
Sominhwan <thalsghks@naver.com>
2024-09-10 23:24:36 +0900

    20240910 SMH [api] api 수정

--- lib/views/more/api/more_api.dart
+++ lib/views/more/api/more_api.dart
@@ -14,7 +14,7 @@ class MoreApi {
 
   Future<String> getNotice(Map<String, dynamic> params) async {
     try {
-      final response = await _dio.get('/api/v1/getEmergencyGuideMapManage', queryParameters: params);
+      await _dio.get('/api/v1/getEmergencyGuideMapManage', queryParameters: params);
 
       return 'test';
     } finally {
