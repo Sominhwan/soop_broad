4469fe0ff1f0ac3981ae83777cd297e8ba4d2fd1
Sominhwan <thalsghks@naver.com>
2025-01-13 22:43:51 +0900

    20250113 SMH [toast] custom toast 수정

--- lib/utils/navigation_service.dart
+++ lib/utils/navigation_service.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class NavigationService {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
   static BuildContext? get globalCtx => navigatorKey.currentContext;
+
   static NavigatorState? get currentNavigator => navigatorKey.currentState;
 }
