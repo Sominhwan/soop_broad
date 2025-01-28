169b44aaeba097b576cba97c036bfa995a1e64e2
Sominhwan <thalsghks@naver.com>
2025-01-28 22:53:35 +0900

    20250128 SMH [toast] custom toast 수정

--- lib/utils/custom_route_observer.dart
+++ lib/utils/custom_route_observer.dart
@@ -25,12 +25,14 @@ class CustomRouteObserver extends RouteObserver<PageRoute> {
   @override
   void didPush(Route route, Route? previousRoute) {
     super.didPush(route, previousRoute);
+
     updateRouteInformation(route, previousRoute);
   }
 
   @override
   void didPop(Route route, Route? previousRoute) {
     super.didPop(route, previousRoute);
+
     updateRouteInformation(previousRoute, route);
   }
 
