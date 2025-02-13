dc680d50794b2ef0253f1c6a41f1dc8555f9cc67
Sominhwan <thalsghks@naver.com>
2025-02-13 23:37:49 +0900

    20250213 SMH [toast] custom toast 수정

--- lib/utils/custom_route_settings.dart
+++ lib/utils/custom_route_settings.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 
 class CustomRouteSettings extends RouteSettings {
-  const CustomRouteSettings({super.name, super.arguments});
+  const CustomRouteSettings({
+    super.name,
+    super.arguments
+  });
 }
